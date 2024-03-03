121e638fac3419717714effddc54f95de76f1369
rkcraushan333 <raushank.ug20.ece@nitp.ac.in>
2024-03-03 22:48:30 +0530

    splitting

--- src/App.js
+++ src/App.js
@@ -1,3 +1,38 @@
+import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import React, { lazy, Suspense } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { CartProvider } from "./components/ContextReducer";
+const Home = lazy(() => import("./screens/Home"));
+const Login = lazy(() => import("./screens/Login"));
+const Signup = lazy(() => import("./screens/Singup"));
+const MyOrder = lazy(() => import("./screens/MyOrder"));
+
+function App() {
+  return (
+    <CartProvider>
+      <Router>
+        <div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
+              <Route exact path="/myOrder" element={<MyOrder />} />
+            </Routes>
+          </Suspense>
+        </div>
+      </Router>
+    </CartProvider>
+  );
+}
+
+export default App;
+
+/*
 import "./App.css";
 import Home from "./screens/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -27,3 +62,5 @@ function App() {
 }
 
 export default App;
+
+*/
