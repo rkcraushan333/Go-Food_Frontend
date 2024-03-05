ee794ebcb6185fcf469323f9f7c55acfc5a3ac32
rkcraushan333 <raushank.ug20.ece@nitp.ac.in>
2024-03-05 12:45:23 +0530

    remvoed lazy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-import React, { lazy, Suspense } from "react";
+// import React, { lazy, Suspense } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { CartProvider } from "./components/ContextReducer";
-const Home = lazy(() => import("./screens/Home"));
-const Login = lazy(() => import("./screens/Login"));
-const Signup = lazy(() => import("./screens/Singup"));
-const MyOrder = lazy(() => import("./screens/MyOrder"));
+// const Home = lazy(() => import("./screens/Home"));
+// const Login = lazy(() => import("./screens/Login"));
+// const Signup = lazy(() => import("./screens/Singup"));
+// const MyOrder = lazy(() => import("./screens/MyOrder"));
+import Home from './screens/Home';
+import Login from './screens/Login';
+import MyOrder from './screens/MyOrder';
+import Signup from './screens/Singup';
 
 function App() {
   return (
     <CartProvider>
       <Router>
         <div>
-          <Routes><Route exact path="/" element={<Home />} />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/signup" element={<Signup />} />
             <Route exact path="/myOrder" element={<MyOrder />} />
